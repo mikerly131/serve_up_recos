33f21ac233b01cafde40afae806e66bf75e7af14
Mike <mike@Zipcoders-MacBook-Pro-83.local>
2023-12-25 00:38:34 -0500

    medication request workflow started, encounter setup done, crud changes

--- app_files/crud.py
+++ app_files/crud.py
@@ -2,6 +2,8 @@
 Create, Read, Update, Delete actions defined to interact with the database
 For this project's MVP, might only need Create and Read
 """
+from uuid import UUID
+
 from sqlalchemy.orm import Session
 from . import models, schemas
 
@@ -23,6 +25,13 @@ def get_provider(db: Session, user_name: str):
     return a_provider
 
 
+# Helper function to get the provider id from the supplied provider user_name
+def get_provider_id(db: Session, user_name: str):
+    a_provider = db.query(models.Provider).filter(models.Provider.user_name == user_name).first()
+    provider_id = a_provider.id
+    return provider_id
+
+
 # Function to login providers (users) of the application, raises exceptions for bad user and invalid password
 def login_provider(db: Session, user_name: str, password: str):
     fake_hashed_password = password + "notreallyhashed"
@@ -37,45 +46,59 @@ def login_provider(db: Session, user_name: str, password: str):
 
 
 # Function to provide a list of patients for the provider to select for an encounter
-def get_patients():
-    pass
-
-
-# Function to select a single user for one of the mocked encounter workflows
-def set_workflow_patient():
-    pass
+def get_patients(db: Session):
+    patients = db.query(models.Patient).all()
+    return patients
 
 
 # Function to get all the data to populate an encounter workflow form
-def get_workflow_data():
-    get_patient_details()
-    get_prescriptions()
-    get_medications()
-    pass
+def get_workflow_data(db: Session, patient_id: UUID):
+    patient_details = get_patient_details(db, patient_id)
+    medications = get_medications(db)
+    workflow_data = [patient_details, medications]
+    return workflow_data
 
 
 # Helper function to get patient details to display in the encounter workflow form
-def get_patient_details():
-    pass
-
-
-# Helper function to
-def get_prescriptions():
-    pass
+def get_patient_details(db: Session, patient_id: UUID):
+    patient_details = db.query(models.Patient).filter(models.Patient.id == patient_id).first()
+    return patient_details
 
 
 # Helper function to get selectable medications for a medication request workflow
-def get_medications():
-    pass
+def get_medications(db: Session):
+    medications = db.query(models.Medication).all()
+    return medications
+
+# UNECESSARY - patient_details returns prescriptions objects
+# def get_prescriptions(db: Session, prescriptions):
+#     prescription_details = []
+#     for prescription in prescriptions:
+#         prescription_info = db.query(models.Prescription).filter(models.Prescription.id == prescription.id).first()
+#         prescription_details.append(prescription_info)
+#     return prescription_details
 
 
 # Function to make a medication request on form submission
-def make_medication_request():
-    pass
+def make_medication_request(db: Session, patient_id: UUID, user_name: str, new_med: int):
+    provider_id = get_provider_id(db, user_name)
+
+    # Create a new MedicationRequest
+    medication_request = models.MedicationRequest(
+        patient_id=patient_id,
+        provider_id=provider_id,
+        new_medication=new_med
+    )
+
+    db.add(medication_request)
+    medication_request.set_current_medication_ids(db)
+    db.commit()
+    db.refresh(medication_request)
+    return medication_request.id
 
 
 # Helper function for medication request to return predictions of drug-drug interactions (ddi)
-def make_ddi_predictions():
+def make_ddi_predictions(db: Session, mr_id: int):
     pass
 
 
--- app_files/main.py
+++ app_files/main.py
@@ -1,7 +1,10 @@
 """
 Main script that runs my application.
 """
+from uuid import UUID
+
 from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -29,36 +32,38 @@ def get_db():
 #   medication order - end template, from here provider can pick another patient or workflow.
 
 
-# Path - GET: base/root page with minimal info and links to regist/login
+# Path - GET: base/root page with minimal info and links to register/login
 @app.get("/")
-async def get_root():
-    return templates.TemplateResponse("index.html", {})
+async def get_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 # Path - GET: page for registering a provider
 @app.get("/providers/register")
-async def get_register_provider():
-    return templates.TemplateResponse("register.html", {})
+async def get_register_provider(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 
-# Path - POST: create a provider if the user_name isn't already registered
-# Maybe this should return login template.
-@app.post("/providers/register", response_model=schemas.Provider)
+# Path - POST: create a provider if the user_name isn't already registered, redirect to login after user registered
+@app.post("/providers/register")
 def register_provider(provider: schemas.ProviderCreate, db: Session = Depends(get_db)):
     a_provider = crud.get_provider(db, provider.user_name)
     if a_provider:
         raise HTTPException(status_code=400, detail="Username already registered")
     else:
-        return crud.register_provider(db, provider)
+        crud.register_provider(db, provider)
+        return RedirectResponse(url="/providers/login", status_code=303)
 
 
 # Path - GET: page for logging in a provider
 @app.get("/providers/login")
-async def get_register_provider():
-    return templates.TemplateResponse("login.html", {})
+async def get_register_provider(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 
 # Path - POST: login a provider, validate user_name and email.
+# Need to replace return a_provider with the actual place to send customer
+# Maybe use oauth or something else to generate session tokens
 @app.post("/providers/login", response_model=schemas.Provider)
 def login_provider(user_name: str, password: str, db: Session = Depends(get_db)):
     a_provider = crud.login_provider(db, user_name, password)
@@ -67,7 +72,40 @@ def login_provider(user_name: str, password: str, db: Session = Depends(get_db))
     elif a_provider == 'User':
         raise HTTPException(status_code=401, detail="Provider not found")
     else:
-        return a_provider
+        redirect_url = f"/encounter/setup?user_name={a_provider.user_name}"
+        return RedirectResponse(url=redirect_url)
+
+
+# Path - GET: Show a logged in provider the workflows and patients they can choose (1 each) to start the workflow
+@app.get("/encounter/setup")
+def setup_encounter(request: Request, user_name: str, db: Session = Depends(get_db)):
+    patients = crud.get_patients(db)
+    workflows = {1: "medication_request"}
+    return templates.TemplateResponse("encounter_setup.html",{
+        "request": request, "provider": user_name, "patients": patients, "workflows": workflows})
+
+
+@app.get("/encounter/{user_name}/{patient_name}/{workflow_name}")
+def start_workflow(request: Request, user_name: str, workflow_name: str,
+                   patient_id: UUID, db: Session = Depends(get_db)):
+    if workflow_name == "medication_request":
+        workflow_data = crud.get_workflow_data(db, patient_id)
+        return templates.TemplateResponse("medication_request_wf.html", {
+            "request": request, "provider": user_name, "workflow_data": workflow_data})
+
+
+@app.post("/encounter/{user_name}/{patient_name}/{workflow_name}")
+def medication_request(request: Request, user_name: str,
+                       patient_id: UUID, new_med: int, db: Session = Depends(get_db)):
+    mr_id = crud.make_medication_request(db, patient_id, user_name, new_med)
+    predictions = crud.make_ddi_predictions(mr_id)
+
+
+    # get predictions
+    # re-render the workflow template with medication requests displayed
+
+
+
 
 
 
